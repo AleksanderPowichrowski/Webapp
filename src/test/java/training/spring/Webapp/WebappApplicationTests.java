45664f3368c65b3347822ab68ee05c2dfc46fc76
aleksander.powichrowski <aleksander.powichrowski@redcompass.com>
2022-08-26 14:45:46 +0200

    Initial commit

---
+++ src/test/java/training/spring/Webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package training.spring.Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
